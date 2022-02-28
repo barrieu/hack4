9eb2ddadced5d61f50a3b6656f1364f9d69d9c47
barrieu <37584427+barrieu@users.noreply.github.com>
2022-02-28 16:12:21 +0000

    Update audioStuff.js

--- audioStuff.js
+++ audioStuff.js
@@ -4,24 +4,24 @@ function playTone(selectPart){
   //var version = jsPsych.version();
   //console.log(version);
   startT = (selectPart * 1) - 1;
-  stopT  = startT + 0.9;
+  stopT  = startT + 0.95;
   console.log("startT = " + startT);
   audio.currentTime = startT;
   audio.play();
 
 // the below setInterval is to check the currentTime
   setInterval(function(){
-    if(audio.currentTime>stopT){
+    if(audio.currentTime > stopT){
       audio.pause();
         }
-      },50);
+      },10);
   }
 
 
 function playList1(vocalList){
   console.log("playing Audio List");
   playTone(vocalList[0]);
-  setTimeout(function(){ playTone(vocalList[1]);}, 1000);
-  setTimeout(function(){ playTone(vocalList[2]);}, 2000);
-  setTimeout(function(){ playTone(vocalList[3]);}, 3000);
+  setTimeout(function(){ playTone(vocalList[1]);}, 1500);
+  setTimeout(function(){ playTone(vocalList[2]);}, 3000);
+  setTimeout(function(){ playTone(vocalList[3]);}, 4500);
 }
